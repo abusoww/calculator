9542177035de16988274bea5a2d1aae28b589782
Nicat Abushov <abusovnicat2008@gmail.com>
2025-05-05 11:47:13 +0400

    1

--- script.js
+++ script.js
@@ -781,7 +781,7 @@ function downloadResults() {
 }
 
 function toggleTheme() {
-    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
     updateStarColors();
 }
 
